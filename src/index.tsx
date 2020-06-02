66b7c45b8ba56ca89c51fa715440d400485698fe
Daniel Rivas <Daniel.Rivas@huddle.com>
2020-06-02 11:14:29 +0100

    Expose usePaginatedResource

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { useResource } from './useResource';
+export { usePaginatedResource } from './usePaginatedResource';
 export { CacheContext, CacheProvider } from './cache-context';
 export { Cache } from './cache';
