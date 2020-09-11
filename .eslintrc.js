a5496ca12486d202756300f98a750efc28d77662
Daniel Rivas <Daniel.Rivas@huddle.com>
2020-09-11 11:04:06 +0100

    Go away dependabot

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-  plugins: ['react-hooks'],
-  extends: ['react-app', 'prettier/@typescript-eslint'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-};
+  "extends": [
+    "react-app",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
